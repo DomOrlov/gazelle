fe198fb8947d7a6778910a07b78d83864e272277
DomOrlov <dominik.orlovskij2@mail.dcu.ie>
2025-04-29 22:54:27 +0100

    Added graph debugging and removed irrelavent parts of the code

--- pfss/iris_get_pfss_utils.py
+++ pfss/iris_get_pfss_utils.py
@@ -370,8 +370,7 @@ def get_pfss_from_map(map, min_gauss = -20, max_gauss = 20, dimension = (1080, 5
     #for i, (f, seed_coord) in enumerate(zip(fieldlines, seeds)): #zip takes two lists and combines them into a list of tuples, enumerate gives an index to each tuple.
     #    f.start_pix = (flat_x[i], flat_y[i]) # Assigns the EIS pixel (x, y) where this fieldline was seeded from.
     # Convert seed world coordinates directly to EIS pixel coordinates
-    seeds_2d = SkyCoord(seeds.lon, seeds.lat, frame=seeds.frame)
-    seeds_2d = seeds_2d.transform_to(map.coordinate_frame)
+    seeds_2d = seeds.transform_to(map.coordinate_frame)
     x_pix, y_pix = map.world_to_pixel(seeds_2d)
     for f, x, y in zip(fieldlines, x_pix, y_pix):
         f.start_pix = (int(x), int(y))  # or round(x), round(y) if needed
