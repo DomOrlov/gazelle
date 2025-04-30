0f947e95eb755e44288c4ab40ad4bc93cfe70065
DomOrlov <dominik.orlovskij2@mail.dcu.ie>
2025-04-30 19:43:51 +0100

    fixing missalignment issue

--- pfss/iris_get_pfss_utils.py
+++ pfss/iris_get_pfss_utils.py
@@ -370,14 +370,15 @@ def get_pfss_from_map(map, min_gauss = -20, max_gauss = 20, dimension = (1080, 5
 
     plt.grid(True)
     plt.show() # Confirms that fieldlines were successfully traced from the seeds.
-
+    print("==========================")
     print("Adding seed metadata to fieldlines...")
+    seeds = SkyCoord(seeds) # Ensure seeds are full SkyCoord objects (not just frame instances) to enable transformation and pixel mapping.
     print("Seed frame before transformation:", seeds.frame.name)
     print("Map frame (EIS):", map.coordinate_frame)
     print("Sample seed Carrington lon/lat before transform:", seeds[0].lon.deg, seeds[0].lat.deg)
-    seeds = SkyCoord(seeds) # Ensure seeds are full SkyCoord objects (not just frame instances) to enable transformation and pixel mapping.
     seeds_2d = seeds.transform_to(map.coordinate_frame) # Convert seed coordinates to the 2D helioprojective frame of the EIS map.
     print("Sample seed Solar-X/Y after transform:", seeds_2d[0].Tx.to(u.arcsec), seeds_2d[0].Ty.to(u.arcsec))
+    print("===========================")
     x_pix, y_pix = map.world_to_pixel(seeds_2d) # Map each transformed seed coordinate to its corresponding (x, y) pixel location on the EIS image.
     x_vals = x_pix.value # Extract raw pixel values from astropy Quantity objects.
     y_vals = y_pix.value 
